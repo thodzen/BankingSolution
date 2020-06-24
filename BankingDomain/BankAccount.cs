f0a5c745bdc02b53e02d20f5bd438b2eb97f7d96
Tyler Hodzen <38892841+thodzen@users.noreply.github.com>
2020-06-24 18:11:47 +0000

    Done with flags

--- BankingDomain/BankAccount.cs
+++ BankingDomain/BankAccount.cs
@@ -2,9 +2,12 @@
 
 namespace BankingDomain
 {
+    public enum AccountType { Standard, Gold }
     public class BankAccount
     {
         private decimal _currentBalance = 5000;
+
+        public AccountType AccountType = AccountType.Standard;
         public decimal GetBalance()
         {
             return _currentBalance;
@@ -12,6 +15,10 @@ namespace BankingDomain
 
         public void Deposit(decimal amountToDeposit)
         {
+            if (AccountType == AccountType.Gold)
+            {
+                amountToDeposit *= 1.10M;
+            }
             _currentBalance += amountToDeposit;
         }
 
---
+++ BankingTests/GoldAccountDepositsTest.cs
@@ -0,0 +1,26 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BankingTests
+{
+    public class GoldAccountDepositsTest
+    {
+        [Fact]
+        public void GoldAccountsGetABonusForDeposits()
+        {
+            var goldAccount = new BankAccount();
+            var openingBalance = goldAccount.GetBalance();
+            var amountToDeposit = 100M;
+            goldAccount.AccountType = AccountType.Gold;
+
+            goldAccount.Deposit(amountToDeposit);
+
+            var expectedBalance = (amountToDeposit * 1.10M) + openingBalance;
+
+            Assert.Equal(expectedBalance, goldAccount.GetBalance());
+        }
+    }
+}
